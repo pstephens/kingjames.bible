a93a5435cc1fe4c714c390ce47779ad3f597e139
Peter Stephens <code@diligentsoftware.com>
2015-07-19 21:44:50 -0500

    #2 - Implemented more node interop. And plugin model.

--- src/normalizer/core.cljs
+++ src/normalizer/core.cljs
@@ -1,9 +1,25 @@
 (ns normalizer.core
-    (:require [cljs.nodejs :as nodejs]))
+    (:require
+      [cljs.nodejs :as nodejs]
+      [normalizer.staggs :as staggs]))
 
 (nodejs/enable-util-print!)
 
-(defn -main [& args]
-    (println "Hello world!"))
+(def process nodejs/process)
 
-(set! *main-cli-fn* -main)
+(def parsers {
+  :staggs staggs/parser
+  })
+
+(defn run-parser [parser path output]
+  (let [p (parsers (keyword parser))]
+    (if p
+      (p path output)
+      (do
+        (println (str "Failed to find parser '" parser "'."))
+        1))))
+
+(defn- main [parser path output]
+  (.exit process (run-parser parser path output)))
+
+(set! *main-cli-fn* main)
---
+++ src/normalizer/staggs.cljs
@@ -0,0 +1,6 @@
+(ns normalizer.staggs
+  (:require [cljs.nodejs :as nodejs]))
+
+(defn parser [path]
+  (println (str "Staggs parser: " path))
+  0)
